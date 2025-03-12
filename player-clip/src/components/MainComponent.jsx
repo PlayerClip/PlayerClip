9ab143ff3d08b35cb3e0e9a5df6a55af4546c69f
vRuShIl13 <patelvk1@myumanitoba.ca>
2025-03-12 14:35:39 -0500

    Trying to add drag and drop, and fixed the constant component boarder.

--- player-clip/src/components/MainComponent.jsx
+++ player-clip/src/components/MainComponent.jsx
@@ -2,17 +2,20 @@ import '../css/MainComponent.css'
 import ConstantComponent from './ConstantComponent.jsx'
 import TabHolder from './TabHolder.jsx'
 import CardComponent from './CardComponent.jsx'
+import Draggable from 'react-draggable'
+
+
 function MainComponent(){
 
     return (
-        <div className='mainComponent'>
-            <div className= 'constantView'>
-                <ConstantComponent/>
+            <div className='mainComponent'>
+                <div className= 'constantView'>
+                    <ConstantComponent/>
+                </div>
+                <div className= 'tabs'>                         
+                    <TabHolder />
+                </div>
             </div>
-            <div className= 'tabs'>
-                <TabHolder />
-            </div>
-        </div>
     )
 }
 export default MainComponent
