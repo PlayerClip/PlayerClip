483bd70de848337f7818a85ac3a99bb620e1a91b
Gabriel Coloma <gabrielzaccipc@gmail.com>
2025-03-11 22:18:14 -0500

    Added draggable

--- player-clip/src/App.jsx
+++ player-clip/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import Draggable from 'react-draggable';
 import './css/App.css'
 
 function App() {
@@ -5,13 +7,11 @@ function App() {
   return (
     <>
     <h1> Welcome to Player Clip!</h1>
-    <div className='mainComponent'>
-
+      <div className='mainComponent'>
       <div className= 'constantView'>
+        <p>Plormby Gillman</p>
       </div>
-
       <div className= 'tabs'>
-        
       </div>
     </div>
     </>
