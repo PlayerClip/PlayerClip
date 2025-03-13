437c6818e1ec4c9eec0f66f386a58ebe675569c4
vRuShIl13 <patelvk1@myumanitoba.ca>
2025-03-12 21:46:54 -0500

    changed the constant component to use fixed card component, something new

--- player-clip/src/components/ConstantComponent.jsx
+++ player-clip/src/components/ConstantComponent.jsx
@@ -1,11 +1,37 @@
+import React, { useState } from 'react'
 import '../css/ConstantComponent.css'
-import CardComponent from './CardComponent'
-import React from 'react'
+import FixedCardComponent from './FixedCardComponent.jsx'
 
 function ConstantComponent() {
+    const [character, setCharacter] = useState({
+        name: 'Plormby Gillman',
+        level: 1,
+        hitpoints: 100,
+        actionClass: '',
+        initiative: 0,
+        inspiration: '',
+        money: 0
+    })
+
+    const handleChange = (field, value) => {
+        setCharacter({
+            ...character,
+            [field]: value
+        })
+    }
+
     return (
-        <div >
-            <CardComponent title="Plormby Gillman" content="Some content here" />
+        <div className="constantView">
+            <FixedCardComponent
+                name={character.name}
+                level={character.level}
+                hitpoints={character.hitpoints}
+                actionClass={character.actionClass}
+                initiative={character.initiative}
+                inspiration={character.inspiration}
+                money={character.money}
+                onChange={handleChange}
+            />
         </div>
     )
 }
--- player-clip/src/components/FixedCardComponent.jsx
+++ player-clip/src/components/FixedCardComponent.jsx
@@ -8,25 +8,27 @@ function FixedCardComponent({ name, level, hitpoints, actionClass, initiative, i
                 <h2>{name}</h2>
                 <p>Level: {level}</p>
             </div>
-            <div className="field">
-                <label>Hitpoints:</label>
-                <input type="number" value={hitpoints} onChange={(e) => onChange('hitpoints', e.target.value)} />
-            </div>
-            <div className="field">
-                <label>Action Class:</label>
-                <input type="text" value={actionClass} onChange={(e) => onChange('actionClass', e.target.value)} />
-            </div>
-            <div className="field">
-                <label>Initiative:</label>
-                <input type="number" value={initiative} onChange={(e) => onChange('initiative', e.target.value)} />
-            </div>
-            <div className="field">
-                <label>Inspiration:</label>
-                <input type="text" value={inspiration} onChange={(e) => onChange('inspiration', e.target.value)} />
-            </div>
-            <div className="field">
-                <label>Money:</label>
-                <input type="number" value={money} onChange={(e) => onChange('money', e.target.value)} />
+            <div className="fields-container">
+                <div className="field">
+                    <label>Hitpoints:</label>
+                    <input type="number" value={hitpoints} onChange={(e) => onChange('hitpoints', e.target.value)} />
+                </div>
+                <div className="field">
+                    <label>Action Class:</label>
+                    <input type="text" value={actionClass} onChange={(e) => onChange('actionClass', e.target.value)} />
+                </div>
+                <div className="field">
+                    <label>Initiative:</label>
+                    <input type="number" value={initiative} onChange={(e) => onChange('initiative', e.target.value)} />
+                </div>
+                <div className="field">
+                    <label>Inspiration:</label>
+                    <input type="text" value={inspiration} onChange={(e) => onChange('inspiration', e.target.value)} />
+                </div>
+                <div className="field">
+                    <label>Money:</label>
+                    <input type="number" value={money} onChange={(e) => onChange('money', e.target.value)} />
+                </div>
             </div>
         </div>
     )
